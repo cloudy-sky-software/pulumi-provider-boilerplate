95693eb7306d0dc3476dff631cd15cb6d122946c
Praneet <1466314+praneetloke@users.noreply.github.com>
2024-01-09 14:39:54 -0800

    Add interface functions to be implemented by the user

--- provider/pkg/provider/provider.go
+++ provider/pkg/provider/provider.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/cloudy-sky-software/pulumi-xyz/provider/pkg/openapi"
 	"github.com/getkin/kin-openapi/openapi3"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/pkg/errors"
 
@@ -96,9 +97,6 @@ func makeProvider(host *provider.HostClient, name, version string, pulumiSchemaB
 
 // Configure is called by the engine to give you a chance to prepare the
 // provider to handle resource requests, such as configuring auth etc.
-//
-// TODO: Implement the remaining functions from the `ResourceProviderServer` interface.
-// That is, Create, Read, Diff, Update, Delete at a very minimum.
 func (p *xyzProvider) Configure(_ context.Context, req *pulumirpc.ConfigureRequest) (*pulumirpc.ConfigureResponse, error) {
 	apiKey, ok := req.GetVariables()["xyz:config:apiKey"]
 	if !ok {
@@ -123,4 +121,112 @@ func (p *xyzProvider) Configure(_ context.Context, req *pulumirpc.ConfigureReque
 	return &pulumirpc.ConfigureResponse{
 		AcceptSecrets: true,
 	}, nil
+}
+
+// GetSchema fetches the schema for this resource provider.
+func (p *xyzProvider) GetSchema(_ context.Context, _ *pulumirpc.GetSchemaRequest) (*pulumirpc.GetSchemaResponse, error) {
+	return &pulumirpc.GetSchemaResponse{schema: p.schema}, nil
+}
+
+// TODO: Implement the remaining functions from the `ResourceProviderServer`
+// interface below. That is, Create, Read, Diff, Update, Delete and Inokve
+// at a minimum.
+
+// CheckConfig validates the configuration for this resource provider.
+func (p *xyzProvider) CheckConfig(_ context.Context, _ *pulumirpc.CheckRequest) (*pulumirpc.CheckResponse, error) {
+	return nil, nil
+}
+
+// DiffConfig checks the impact a hypothetical change to this provider's configuration will have on the provider.
+func (p *xyzProvider) DiffConfig(_ context.Context, _ *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
+	return nil, nil
+}
+
+// Invoke dynamically executes a built-in function in the provider.
+func (p *xyzProvider) Invoke(_ context.Context, _ *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+	return nil, nil
+}
+
+// StreamInvoke dynamically executes a built-in function in the provider, which returns a stream
+// of responses.
+func (p *xyzProvider) StreamInvoke(_ *pulumirpc.InvokeRequest, _ pulumirpc.ResourceProvider_StreamInvokeServer) error {
+	return nil
+}
+
+// Call dynamically executes a method in the provider associated with a component resource.
+func (p *xyzProvider) Call(_ context.Context, _ *pulumirpc.CallRequest) (*pulumirpc.CallResponse, error) {
+	return nil, nil
+}
+
+// Check validates that the given property bag is valid for a resource of the given type and returns the inputs
+// that should be passed to successive calls to Diff, Create, or Update for this resource. As a rule, the provider
+// inputs returned by a call to Check should preserve the original representation of the properties as present in
+// the program inputs. Though this rule is not required for correctness, violations thereof can negatively impact
+// the end-user experience, as the provider inputs are using for detecting and rendering diffs.
+func (p *xyzProvider) Check(_ context.Context, _ *pulumirpc.CheckRequest) (*pulumirpc.CheckResponse, error) {
+	return nil, nil
+}
+
+// Diff checks what impacts a hypothetical update will have on the resource's properties.
+func (p *xyzProvider) Diff(_ context.Context, _ *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
+	return nil, nil
+}
+
+// Create allocates a new instance of the provided resource and returns its unique ID afterwards.  (The input ID
+// must be blank.)  If this call fails, the resource must not have been created (i.e., it is "transactional").
+func (p *xyzProvider) Create(_ context.Context, _ *pulumirpc.CreateRequest) (*pulumirpc.CreateResponse, error) {
+	return nil, nil
+}
+
+// Read the current live state associated with a resource.  Enough state must be include in the inputs to uniquely
+// identify the resource; this is typically just the resource ID, but may also include some properties.
+func (p *xyzProvider) Read(_ context.Context, _ *pulumirpc.ReadRequest) (*pulumirpc.ReadResponse, error) {
+	return nil, nil
+}
+
+// Update updates an existing resource with new values.
+func (p *xyzProvider) Update(_ context.Context, _ *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
+	return nil, nil
+}
+
+// Delete tears down an existing resource with the given ID.  If it fails, the resource is assumed to still exist.
+func (p *xyzProvider) Delete(_ context.Context, _ *pulumirpc.DeleteRequest) (*emptypb.Empty, error) {
+	return nil, nil
+}
+
+// Construct creates a new instance of the provided component resource and returns its state.
+func (p *xyzProvider) Construct(_ context.Context, _ *pulumirpc.ConstructRequest) (*pulumirpc.ConstructResponse, error) {
+	return nil, nil
+}
+
+// Cancel signals the provider to gracefully shut down and abort any ongoing resource operations.
+// Operations aborted in this way will return an error (e.g., `Update` and `Create` will either return a
+// creation error or an initialization error). Since Cancel is advisory and non-blocking, it is up
+// to the host to decide how long to wait after Cancel is called before (e.g.)
+// hard-closing any gRPC connection.
+func (p *xyzProvider) Cancel(_ context.Context, _ *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, nil
+}
+
+// GetPluginInfo returns generic information about this plugin, like its version.
+func (p *xyzProvider) GetPluginInfo(_ context.Context, _ *emptypb.Empty) (*pulumirpc.PluginInfo, error) {
+	return nil, nil
+}
+
+// Attach sends the engine address to an already running plugin.
+func (p *xyzProvider) Attach(_ context.Context, _ *pulumirpc.PluginAttach) (*emptypb.Empty, error) {
+	return nil, nil
+}
+
+// GetMapping fetches the mapping for this resource provider, if any. A provider should return an empty
+// response (not an error) if it doesn't have a mapping for the given key.
+func (p *xyzProvider) GetMapping(_ context.Context, _ *pulumirpc.GetMappingRequest) (*pulumirpc.GetMappingResponse, error) {
+	return nil, nil
+}
+
+// GetMappings is an optional method that returns what mappings (if any) a provider supports. If a provider does not
+// implement this method the engine falls back to the old behaviour of just calling GetMapping without a name.
+// If this method is implemented than the engine will then call GetMapping only with the names returned from this method.
+func (p *xyzProvider) GetMappings(_ context.Context, _ *pulumirpc.GetMappingsRequest) (*pulumirpc.GetMappingsResponse, error) {
+	return nil, nil
 }
